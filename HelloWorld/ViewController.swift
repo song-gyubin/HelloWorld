d3004345071a0254f07576ad4d7ddee08741217a
song-gyubin <song_gyubin@kakaku.com>
2021-04-19 13:24:20 +0900

    IBaction IBoutlet

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func handle(_ sender: Any) {
+        label.text = "Swift"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
